7984aef0fcad7bf21743d3d65f8c53515c4a03bc
TeodoraUrsu <teodoraursu@yahoo.com>
2024-03-19 00:47:03 +0200

    no message

--- main.py
+++ main.py
@@ -1,96 +1,177 @@
-import json
-import os
-import shutil
-
+import numpy as np
 import pandas as pd
-import seaborn as sns
+import tensorflow as tf
+from PIL import Image
+from keras import optimizers
+from keras.layers import Activation, BatchNormalization, Convolution2D, Dense, Dropout, Flatten, MaxPooling2D
+from keras.models import Sequential
 from keras.preprocessing.image import ImageDataGenerator
-from scipy.stats._mstats_basic import trim
-
-sns.set_style('darkgrid')
+from matplotlib import pyplot as plt
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
-import configparser
+
+PARENT_DIR = "C:/Users/teodo/PycharmProjects/pythonProject1"
+IMG_DIR = f"{PARENT_DIR}/bee_imgs/bee_imgs/"
+CSV_DIR = f"{PARENT_DIR}/bee_data.csv"
+epochs = 50
+df = pd.read_csv(CSV_DIR)
+
+X_pics = [Image.open(f"{IMG_DIR}{img_name}").convert('RGB').resize((64, 64)) for img_name in df["file"]]
+X = [np.array(img) / 255.0 for img in X_pics]
+
+y_keys = {
+    "healthy": np.array([1, 0, 0, 0, 0, 0]),
+    "few varrao, hive beetles": np.array([0, 1, 0, 0, 0, 0]),
+    "Varroa, Small Hive Beetles": np.array([0, 0, 1, 0, 0, 0]),
+    "ant problems": np.array([0, 0, 0, 1, 0, 0]),
+    "hive being robbed": np.array([0, 0, 0, 0, 1, 0]),
+    "missing queen": np.array([0, 0, 0, 0, 0, 1])
+}
+y = [y_keys[label] for label in df["health"]]
 
 
-with open('config.json') as f:
-    config = json.load(f)
-
-parent_dir = config['Paths']['parent_dir']
-img_dir = config['Paths']['img_dir']
-csv_dir = config['Paths']['csv_dir']
-
-df = pd.read_csv(csv_dir)
-df = df.drop(['date', 'time', 'location', 'zip code', 'health', 'pollen_carrying', 'caste'], axis=1)
-df.columns = ['filepaths', 'labels']
-df['filepaths'] = df['filepaths'].apply(lambda x: os.path.join(img_dir, x))
-
-train_df, dummy_df = train_test_split(df, train_size=.8, shuffle=True, random_state=123, stratify=df['labels'])
-valid_df, test_df = train_test_split(dummy_df, train_size=.5, shuffle=True, random_state=123,
-                                     stratify=dummy_df['labels'])
-print('train_df lenght: ', len(train_df), '  test_df length: ', len(test_df), '  valid_df length: ', len(valid_df))
-groups = train_df.groupby('labels')
-class_count = len(groups)
-print(' There are ', class_count, ' classes in the dataframe')
-print('{0:^30s} {1:^13s}'.format('CLASS', 'IMAGE COUNT'))
-for label in train_df['labels'].unique():
-    group = groups.get_group(label)
-    print('{0:^30s} {1:^13s}'.format(label, str(len(group))))
-
-max_samples = 400
-min_samples = 0
-column = 'labels'
-train_df = trim(train_df, max_samples, min_samples, column)
+def random_imgs(data_frame, num_images, x_pics):
+    index_lst = data_frame["file"].sample(n=num_images, random_state=1).index
+    return [x_pics[i] for i in index_lst]
 
 
-def balance(train_df, max_samples, min_samples, column, working_dir, image_size):
-    train_df = train_df.copy()
-    aug_dir = os.path.join(working_dir, 'aug')
-    if os.path.isdir(aug_dir):
-        shutil.rmtree(aug_dir)
-    os.mkdir(aug_dir)
-    for label in train_df['labels'].unique():
-        dir_path = os.path.join(aug_dir, label)
-        os.mkdir(dir_path)
-    total = 0
-    gen = ImageDataGenerator(horizontal_flip=True, rotation_range=20, width_shift_range=.2,
-                             height_shift_range=.2, zoom_range=.2)
-    groups = train_df.groupby('labels')
-    for label in train_df['labels'].unique():
-        group = groups.get_group(label)
-        sample_count = len(group)
-        if sample_count < max_samples:
-            aug_img_count = 0
-            delta = max_samples - sample_count
-            target_dir = os.path.join(aug_dir, label)
-            aug_gen = gen.flow_from_dataframe(group, x_col='filepaths', y_col=None, target_size=image_size,
-                                              class_mode=None, batch_size=1, shuffle=False,
-                                              save_to_dir=target_dir, save_prefix='aug-', color_mode='rgb',
-                                              save_format='jpg')
-            while aug_img_count < delta:
-                images = next(aug_gen)
-                aug_img_count += len(images)
-            total += aug_img_count
-    print('Total Augmented images created= ', total)
-    if total > 0:
-        aug_fpaths = []
-        aug_labels = []
-        classlist = os.listdir(aug_dir)
-        for klass in classlist:
-            classpath = os.path.join(aug_dir, klass)
-            flist = os.listdir(classpath)
-            for f in flist:
-                fpath = os.path.join(classpath, f)
-                aug_fpaths.append(fpath)
-                aug_labels.append(klass)
-        Fseries = pd.Series(aug_fpaths, name='filepaths')
-        Lseries = pd.Series(aug_labels, name='labels')
-        aug_df = pd.concat([Fseries, Lseries], axis=1)
-        train_df = pd.concat([train_df, aug_df], axis=0).reset_index(drop=True)
-
-    print(list(train_df['labels'].value_counts()))
-    return train_df
+def plot_bees(img_lst, title):
+    fig, ax = plt.subplots(nrows=1, ncols=4, figsize=(8, 8))
+    for i, img in enumerate(img_lst):
+        ax[i].imshow(img)
+    plt.title(title)
+    plt.show()
 
 
-working_dir = r'./'
-img_size = (70, 70)
-train_df = balance(train_df, max_samples, min_samples, column, working_dir, img_size)
+healthy = random_imgs(df[df["health"] == "healthy"], 4, X_pics)
+hive_beetles = random_imgs(df[df["health"] == "few varrao, hive beetles"], 4, X_pics)
+ant_probs = random_imgs(df[df["health"] == "ant problems"], 4, X_pics)
+hive_robbed = random_imgs(df[df["health"] == "hive being robbed"], 4, X_pics)
+varroa = random_imgs(df[df["health"] == "Varroa, Small Hive Beetles"], 4, X_pics)
+
+plot_bees(healthy, "healthy")
+plot_bees(hive_beetles, "few varrao, hive beetles")
+plot_bees(ant_probs, "ant problems")
+plot_bees(hive_robbed, "hive being robbed")
+plot_bees(varroa, "Varroa, Small Hive Beetles")
+
+history = tf.keras.callbacks.History()
+
+
+def train_cnn():
+    model = Sequential()
+    model.add(Convolution2D(11, 3, 3, input_shape=(64, 64, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2), padding="SAME"))
+    model.add(Convolution2D(21, 3, 3, activation="relu"))
+    model.add(BatchNormalization())
+    model.add(Activation("relu"))
+    model.add(MaxPooling2D(pool_size=(2, 2), padding="SAME"))
+    model.add(Flatten())
+    model.add(Dense(200, activation="relu"))
+    model.add(Dropout(0.2))
+    model.add(Dense(6, activation="softmax"))
+
+    model.compile(optimizer=optimizers.RMSprop(lr=0.0001), loss="categorical_crossentropy", metrics=["accuracy"])
+    return model
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=1)
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, stratify=y_train, random_state=1)
+
+
+def model_plot(history, title):
+    y_range = [0.5, 1.0]
+
+    plt.plot(history.history["loss"])
+    plt.legend(["Train Loss"])
+    plt.title(title + " - Loss")
+    plt.show()
+
+    plt.plot(history.history["accuracy"])
+    plt.legend(["Train Accuracy"])
+    plt.title(title + " - Accuracy")
+    plt.ylim(y_range)
+    plt.show()
+
+
+def data_aug(X_train):
+    datagen = ImageDataGenerator(
+        rotation_range=20,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        vertical_flip=True,
+        width_shift_range=0.3,
+        height_shift_range=0.3
+    )
+
+    datagen.fit(X_train)
+    return datagen
+
+
+model1 = train_cnn()
+history1 = model1.fit(np.array(X_train), np.array(y_train), validation_data=(np.array(X_val), np.array(y_val)),
+                      verbose=True, shuffle=True, epochs=epochs)
+
+datagen = data_aug(X_train)
+
+model2 = train_cnn()
+history2 = model2.fit_generator(datagen.flow(np.array(X_train), np.array(y_train), batch_size=50),
+                                validation_data=(np.array(X_val), np.array(y_val)),
+                                steps_per_epoch=len(X_train) / 50, epochs=epochs)
+
+model_plot(history1, title="Model 1")
+model_plot(history2, title="Model 2 with Augmentation")
+
+def multi_pred(models, X_test, y_test):
+    preds = [model.predict(np.array(X_test)) for model in models]
+    cols = [f"model{i}_prediction" for i in range(1, len(models) + 1)]
+    preds_df = pd.DataFrame(data=np.argmax(np.array(preds).mean(axis=0), axis=1),
+                            columns=["ensemble_prediction"])
+
+    for i, col in enumerate(cols):
+        preds_df[col] = np.argmax(np.array(preds[i]), axis=1)
+
+    preds_df["target"] = np.argmax(np.array(y_test), axis=1)
+    return preds_df
+
+
+preds = multi_pred([model1, model2], X_test, y_test)
+print(preds)
+accuracy_1 = accuracy_score(preds["model1_prediction"], preds["target"])
+print("Accuracy for Model 1:", accuracy_1)
+
+accuracy_2 = accuracy_score(preds["model2_prediction"], preds["target"])
+print("Accuracy for Model 2:", accuracy_2)
+
+
+def accuracy_table(df):
+    models_lst = df.columns.tolist()  # Putting model names into a list
+    models_lst.remove("target")
+    health_cols = ["healthy", "few varrao, hive beetles", "Varroa, Small Hive Beetles", "ant problems",
+                   "hive being robbed", "missing queen"]
+    df_acc = []
+
+    for model in models_lst:
+        acc_lst = []
+        for i in range(6):
+            size = (df["target"] == i).sum()
+            true = ((df[model] == i) & (df["target"] == i)).sum()
+            acc_lst.append(true / size)
+        df_acc.append(acc_lst)
+
+    df_acc = pd.DataFrame(df_acc, columns=health_cols)
+    df_acc.index = models_lst
+    return df_acc
+
+
+df_acc = accuracy_table(preds)
+print("Accuracy Table:")
+print(df_acc)
+
+accuracy_file_path = "accuracy_table.csv"
+df_acc.to_csv(accuracy_file_path)
+
+print(f"Accuracy table saved to {accuracy_file_path}")
